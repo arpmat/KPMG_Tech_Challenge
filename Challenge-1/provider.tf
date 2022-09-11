b5a0b3a869f68ad4539c0ebfe4d629e21ced4077
arpmat <arpmat@gmail.com>
2022-09-11 16:16:36 +0530

    Update provider.tf

--- Challenge-1/provider.tf
+++ Challenge-1/provider.tf
@@ -2,4 +2,7 @@ provider "aws" {
   source = "hashicorp/aws"
   region  = "${var.region}"
   profile = "infra-provisioning"
+  assume_role = {
+    role_arn = "${var.workspace_iam_roles[terraform.workspace]}"
+  }
 }
